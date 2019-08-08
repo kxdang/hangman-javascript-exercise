5ac40071001a4424135fd457c33bcd18c0058fca
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-08-08 13:23:11 -0400

    adding http request to get new word from mead server

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-// HTTP (Hypertext Tranfer Protcol)
-// Request - What do we want to do?
-// Response -  what was actually done
-
 const Game1 = new Hangman("car parts", 4);
 document.querySelector("#word").textContent = Game1.Puzzle;
 document.querySelector("#guesses-left").textContent = Game1.statusMessage;
 
-window.addEventListener("keypress", function(e) {
+window.addEventListener("keypress", e => {
   const guess = String.fromCharCode(e.charCode);
   Game1.makeGuess(guess);
   document.querySelector("#word").textContent = Game1.Puzzle;
   document.querySelector("#guesses-left").textContent = Game1.statusMessage;
 });
+
+//Making an HTTP request
+const request = new XMLHttpRequest();
+const proxy = "https://cors-anywhere.herokuapp.com/";
+request.addEventListener("readystatechange", e => {
+  if (e.target.readyState === 4) {
+    const data = JSON.parse(e.target.responseText);
+    console.log(data);
+  }
+});
+
+request.open("GET", `${proxy}http://puzzle.mead.io/puzzle`);
+request.send();
