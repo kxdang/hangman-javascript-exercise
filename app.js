50756985721fb9353bfbd35c425c07c35c4936b7
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-08-14 12:39:26 -0400

    Callback function pattern added

--- app.js
+++ app.js
@@ -9,38 +9,33 @@ window.addEventListener("keypress", e => {
   document.querySelector("#guesses-left").textContent = Game1.statusMessage;
 });
 
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
+    console.log(puzzle);
+  }
+});
+
 //Making an HTTP request
-const request = new XMLHttpRequest();
-const proxy = "https://cors-anywhere.herokuapp.com/";
-request.addEventListener("readystatechange", e => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText);
-    console.log(data);
-  } else if (e.target.readyState === 4) {
-    console.log("An error has taken place");
-  }
-});
 
-request.open("GET", `${proxy}http://puzzle.mead.io/puzzle?wordCount=3`);
-request.send();
+// //1. Make a new request for all countries
+// //2. Parse the responseText to get back the array of objects
+// //3. Find your country object by its country code (alpha2Code property)
+// //4. Print the full country name (name property)
 
-//1. Make a new request for all countries
-//2. Parse the responseText to get back the array of objects
-//3. Find your country object by its country code (alpha2Code property)
-//4. Print the full country name (name property)
+// const countryCode = "CA";
+// const requestCanada = new XMLHttpRequest();
 
-const countryCode = "CA";
-const requestCanada = new XMLHttpRequest();
+// requestCanada.addEventListener("readystatechange", e => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText);
+//     const country = data.find(country => country.alpha2Code === countryCode);
+//     console.log(country.name);
+//   } else if (e.target.readyState === 4) {
+//     console.log("unable to fetch data");
+//   }
+// });
 
-requestCanada.addEventListener("readystatechange", e => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText);
-    const country = data.find(country => country.alpha2Code === countryCode);
-    console.log(country.name);
-  } else if (e.target.readyState === 4) {
-    console.log("unable to fetch data");
-  }
-});
-
-requestCanada.open("GET", "http://restcountries.eu/rest/v2/all");
-requestCanada.send();
+// requestCanada.open("GET", "http://restcountries.eu/rest/v2/all");
+// requestCanada.send();
