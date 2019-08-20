d3a534fe2982c4bc55a21fbd63f778955e726de3
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-08-20 13:38:49 -0400

    deleting comments

--- requests.js
+++ requests.js
@@ -24,22 +24,3 @@ const getCountry = countryCode => {
     })
     .then(data => data.find(country => country.alpha2Code === countryCode));
 };
-
-// new Promise((resolve, reject) => {
-//   const requestCountry = new XMLHttpRequest();
-
-//   requestCountry.addEventListener("readystatechange", e => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//       const data = JSON.parse(e.target.responseText);
-//       const country = data.find(
-//         country => country.alpha2Code === countryCode
-//       );
-//       resolve(country);
-//     } else if (e.target.readyState === 4) {
-//       reject("Unable to fetch data");
-//     }
-//   });
-
-//   requestCountry.open("GET", "http://restcountries.eu/rest/v2/all");
-//   requestCountry.send();
-// });
