c9ed3ecc06fecfae0ebf8b334914faf2308d8103
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-08-22 14:49:34 -0400

    completed hangman game!

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const word = document.querySelector("#word");
 const guessesLeft = document.querySelector("#guesses-left");
 let Game1;
 
-// word.textContent = Game1.Puzzle;
-// guessesLeft.textContent = Game1.statusMessage;
-
-// window.addEventListener("keypress", e => {
-//   const guess = String.fromCharCode(e.charCode);
-//   Game1.makeGuess(guess);
-//   word.textContent = Game1.Puzzle;
-//   guessesLeft.textContent = Game1.statusMessage;
-// });
+window.addEventListener("keypress", e => {
+  const guess = String.fromCharCode(e.charCode);
+  Game1.makeGuess(guess);
+  word.textContent = Game1.Puzzle;
+  guessesLeft.textContent = Game1.statusMessage;
+});
 
 const render = () => {
   word.textContent = Game1.Puzzle;
